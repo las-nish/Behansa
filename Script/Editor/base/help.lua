860a07172357d0f8f0176658b02fc78ba94d45e0
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2024-03-18 11:15:23 +0530

    Add shortcut keys help

--- Script/Editor/base/help.lua
+++ Script/Editor/base/help.lua
@@ -5,7 +5,10 @@ function M.ShowHelp()
 end
 
 function M.ShortcutKeys()
-    print("Behansa IDE Shortcut Keys\n")
+    local data_file = os.getenv("BEHANSA_CONFIGURATION") .. "data/key_info.dat"
+    local get_content = require("file").GetFileContent(data_file)
+
+    print(get_content)
 end
 
 return M
