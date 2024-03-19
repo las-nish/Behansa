c193ba96333accce526395bb05de1d4ee99513f2
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2024-03-19 16:24:54 +0530

    Add help information

--- Script/Editor/base/help.lua
+++ Script/Editor/base/help.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 function M.ShowHelp()
-    print("Behansa IEditorDE Help Suite\n")
+    local data_file = os.getenv("BEHANSA_CONFIGURATION") .. "data/help_info.dat"
+    local get_content = require("file").GetFileContent(data_file)
+
+    print(get_content)
 end
 
 function M.ShortcutKeys()
